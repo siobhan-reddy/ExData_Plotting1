4202bf94543a8bd516c86476e910e3798aad57ae
siobhan-reddy <37274885+siobhan-reddy@users.noreply.github.com>
2019-01-03 16:59:10 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -20,18 +20,24 @@ data_subset$datetime <- as.POSIXct(datetime)
 ###########################################################################################
 #create plot
 
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+par(mfrow=c(2,2), mar=c(4,4,2,1))
+
 with(data_subset, {
   plot(Global_active_power~datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
+  
   plot(Voltage~Datetime, type="l", 
        ylab="Voltage (volt)", xlab="")
+  
   plot(Sub_metering_1~datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
+  
   lines(Sub_metering_2~datetime,col='Red')
   lines(Sub_metering_3~datetime,col='Blue')
+  
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  
   plot(Global_reactive_power~datetime, type="l", 
        ylab="Global Rective Power (kilowatts)",xlab="")
 })
